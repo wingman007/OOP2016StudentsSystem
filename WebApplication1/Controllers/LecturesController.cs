7953d237ed802cb422e3fa5f7e659c613829c954
backboxx <jivkojelev81@gmail.com>
2016-06-06 15:10:13 +0300

    Lecturers controller. UCN validation

---
+++ WebApplication1/Controllers/LecturesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LecturesController : Controller
+    {
+        private StudentSystemDbContext db = new StudentSystemDbContext();
+
+        // GET: Lectures
+        public ActionResult Index()
+        {
+            return View(db.Lecturs.ToList());
+        }
+
+        // GET: Lectures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lecturs.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lectures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Kabinet,Modile,LastName,EGN,Email,Name")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lecturs.Add(lecture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lecture);
+        }
+
+        // GET: Lectures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lecturs.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Kabinet,Modile,LastName,EGN,Email,Name")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lecture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lecturs.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lecture lecture = db.Lecturs.Find(id);
+            db.Lecturs.Remove(lecture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
