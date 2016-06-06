0fb4dc889e50aa54b40d74ef2adee64ce7367841
ikaradimova <ikaradimova@gmail.com>
2016-06-06 15:47:14 +0300

    Inspector - Irina

---
+++ WebApplication1/Repository/InspectorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repository
+{
+    class InspectorRepository: GenericRepository<Inspector>
+    {
+        public InspectorRepository()
+            :base()
+        { }
+    }
+}
